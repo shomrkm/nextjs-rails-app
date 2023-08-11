732c28f9eab433e8ae5bd551e55c7da6a61a984d
shomrkm <shochan1985@gmail.com>
2023-08-11 11:26:02 +0900

    feat: Add ActionDispatch middleware for omniauth.

--- api/config/application.rb
+++ api/config/application.rb
@@ -23,5 +23,11 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    # For Omniauth
+    # https://kossy-web-engineer.hatenablog.com/entry/2021/08/09/113943
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies  # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
   end
 end
