a27ccdea5aa584ead50bd56e0b7c31ce3c3568c9
shomrkm <shochan1985@gmail.com>
2023-08-12 17:49:08 +0900

    feat: add UsersController.

---
+++ api/app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def create
+        user = User.find_or_creat_by(provider: params[:provider], uid: @params[:uid], name: params[:name], email: params[:email])
+        if user
+          head :ok
+        else
+          render json: { error: "Login failed"}, status: :unprocessable_entity
+        end
+        rescue StandardError => e
+          render json: { error: e.message }, status: :internal_server_error
+        end
+      end
+
+      def destroy
+        user = User.find_by(email: params[:email])
+        if user
+          user.destroy
+        else
+          render json: { error: "User not found" }, status: :not_found
+        end
+      end
+    end
+  end
+end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  post 'auth/:provider/callback', to: 'api/v1/users#create'
+  delete 'users/:email', to: 'api/v1/users#destroy', constraints: { email: %r{[^/]+} }
 end
