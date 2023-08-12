9362f243128b223334b3f422a150c578d77b3276
shomrkm <shochan1985@gmail.com>
2023-08-12 17:48:43 +0900

    feat: update schema.

--- api/db/migrate/20230812081643_create_users.rb
+++ api/db/migrate/20230812081643_create_users.rb
@@ -9,6 +9,8 @@ class CreateUsers < ActiveRecord::Migration[7.0]
 
       t.datetime "created_at", null: false
       t.datetime "updated_at", null: false
+
+      add_index :users, [:provider, :uid], unique: true
     end
   end
 end
