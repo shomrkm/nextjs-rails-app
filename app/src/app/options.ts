50b19353c037b38540b1deffd44c6ccf3caa960a
shomrkm <shochan1985@gmail.com>
2023-08-12 18:01:53 +0900

    feat: add callback to auth if user sign in.

--- app/src/app/options.ts
+++ app/src/app/options.ts
@@ -1,5 +1,8 @@
+import axios from "axios";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+
+const apiUrl = process.env.NEXT_PUBLIC_API_URL;
 
 export const options: NextAuthOptions = {
   debug: true,
@@ -11,7 +14,28 @@ export const options: NextAuthOptions = {
     }),
   ],
   callbacks: {
-    jwt: async ({ token, user, account, profile }) => {
+    signIn: async ({ user, account, profile }) => {
+      const provider = account?.provider;
+      const uid = account?.sub;
+      const name = user.name;
+      const email = user.email;
+
+      try {
+        const response = await axios.post(
+          `${apiUrl}/auth/${provider}/callback`,
+          { provider, uid, name, email }
+        );
+
+        if (response.status !== 200) {
+          return false;
+        }
+        return true;
+      } catch (error) {
+        console.log("Error: ", error);
+        return false;
+      }
+    },
+    jwt: async ({ token, user, account }) => {
       // console.log("in jwt", { user, token, account, profile });
 
       if (user) {
