bc43185c41fa672aff369adb828532a106efcd3c
shomrkm <shochan1985@gmail.com>
2023-08-12 11:57:56 +0900

    feat: update NextAuthOptions.

--- app/src/app/options.ts
+++ app/src/app/options.ts
@@ -2,18 +2,31 @@ import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
 export const options: NextAuthOptions = {
+  debug: true,
+  session: { strategy: "jwt" },
   providers: [
     GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
   callbacks: {
-    jwt: async (token) => {
+    jwt: async ({ token, user, account, profile }) => {
+      console.log("in jwt", { user, token, account, profile });
+
+      if (user) {
+        token.user = user;
+        const u = user as any;
+        token.role = u.role;
+      }
+      if (account) {
+        token.accessToken = account.access_token;
+      }
       return token;
     },
     session: ({ session, token }) => {
       console.log("in session", { session, token });
+
       token.accessToken;
       return {
         ...session,
