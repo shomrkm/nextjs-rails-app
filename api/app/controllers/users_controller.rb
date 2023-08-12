ca26f3322be527a0d9de5c6fb902800688d77359
shomrkm <shochan1985@gmail.com>
2023-08-12 18:01:18 +0900

    refact: update UsersController.

--- api/app/controllers/users_controller.rb
+++ api/app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ module Api
   module V1
     class UsersController < ApplicationController
       def create
-        user = User.find_or_creat_by(provider: params[:provider], uid: @params[:uid], name: params[:name], email: params[:email])
+        user = User.find_or_create_by(provider: params[:provider], uid: @params[:uid], name: params[:name], email: params[:email])
         if user
           head :ok
         else
@@ -19,6 +19,9 @@ module Api
           user.destroy
         else
           render json: { error: "User not found" }, status: :not_found
+        end
+        rescue StandardError => e
+          render json: { error: e.message }, status: :internal_server_error
         end
       end
     end
