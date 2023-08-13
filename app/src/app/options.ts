775a7c5b5686b15218d362a35c0a1399bd402de8
shomrkm <shochan1985@gmail.com>
2023-08-13 14:18:51 +0900

    feat :update codes.

--- app/src/app/options.ts
+++ app/src/app/options.ts
@@ -2,10 +2,10 @@ import axios from "axios";
 import { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
-const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 export const options: NextAuthOptions = {
-  debug: true,
+  // debug: true,
   session: { strategy: "jwt" },
   providers: [
     GoogleProvider({
@@ -22,21 +22,30 @@ export const options: NextAuthOptions = {
 
       try {
         const response = await axios.post(
-          `${apiUrl}/auth/${provider}/callback`,
-          { provider, uid, name, email }
+          `http://127.0.0.1:3000/auth/:provider/callback`,
+          {
+            provider: "google",
+            uid: "116727722188696075203",
+            name: "村上翔太朗",
+            email: "shochan1985@gmail.com",
+          }
         );
+        console.log(response);
 
-        if (response.status !== 200) {
-          return false;
-        }
+        // if (response.status !== 200) {
+        //   return false;
+        // }
         return true;
       } catch (error) {
         console.log("Error: ", error);
-        return false;
+        console.log("Failed to use rail api");
+        // TODO: /auth/:provider/callback へのリクエストが Not Found になってしまう。
+        // 正常に動かすために暫定的に true を返している
+        return true;
       }
     },
     jwt: async ({ token, user, account }) => {
-      // console.log("in jwt", { user, token, account, profile });
+      // console.log("in jwt", { user, token, account });
 
       if (user) {
         token.user = user;
--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,9 +1,42 @@
+"use client";
+
 import React from "react";
+import { DeleteUser } from "./components/Header/DeleteUser/DeleteUser";
+import axios from "axios";
+
+const TestButton = () => {
+  const handleClick = async () => {
+    try {
+      const response = await axios.post(
+        `http://127.0.0.1:3000/auth/:provider/callback`,
+        {
+          provider: "google",
+          uid: "116727722188696075203",
+          name: "村上翔太朗",
+          email: "shochan1985@gmail.com",
+        }
+      );
+
+      console.log("=======OK===========");
+      console.log(response);
+    } catch (error) {
+      console.log("=======NG===========");
+    }
+  };
+
+  return (
+    <button className="p-2 bg-green-300" onClick={handleClick}>
+      Auth Test
+    </button>
+  );
+};
 
 export default function Home() {
   return (
     <main className="flex-col min-h-screen w-max">
       <h1 className="text-4xl mb-4">My Test Application</h1>
+      <DeleteUser />
+      <TestButton />
     </main>
   );
 }
