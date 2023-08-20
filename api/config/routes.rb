ea9c27dacb53b0d959cebf747a906baee58aae9a
shomrkm <shochan1985@gmail.com>
2023-08-20 10:50:54 +0900

    feat: update api route path.

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  resources :users
   resources :events
-  namespace :api do
-    namespace :v1 do
-      resources :users
-    end
-  end
-  post 'auth/:provider/callback', to: 'api/v1/users#create'
-  delete 'users/:email', to: 'api/v1/users#destroy', constraints: { email: %r{[^/]+} }
+
+  post 'auth/:provider/callback', to: 'users#create'
+  delete 'users/:email', to: 'users#destroy', constraints: { email: %r{[^/]+} }
 end
