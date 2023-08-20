25843e0a203c65248f806ec9c7f94677562f6885
shomrkm <shochan1985@gmail.com>
2023-08-20 09:49:14 +0900

    feat: add event model.

--- api/app/models/event.rb
+++ api/app/models/event.rb
@@ -1,2 +1,18 @@
 class Event < ApplicationRecord
+    validates :name, length: { maximum: 50 }, presence: true
+    validates :place, length: { maximum: 100 }, presence: true
+    validates :content, length: { maximum: 2000 }, presence: true
+    validates :start_at, presence: true
+    validates :end_at, presence: true
+    validate :start_at_should_be_before_end_at
+
+    private
+    
+    def start_at_should_be_before_end_at
+        return unless start_at && end_at
+
+        if start_at >= end_at
+            errors.add(:start_at, "must be set before the end time")
+        end
+    end
 end
