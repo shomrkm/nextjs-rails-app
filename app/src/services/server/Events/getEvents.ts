8202efb0e845c026edd14364180aef23e6cff2ff
shomrkm <shochan1985@gmail.com>
2023-09-10 10:32:32 +0900

    fix: fix error handling in frontend.

--- app/src/services/server/Events/getEvents.ts
+++ app/src/services/server/Events/getEvents.ts
@@ -1,16 +1,13 @@
 import axios from "@/lib/axios/axios";
-import { cookies } from "next/headers";
 import { buildAuthorizationHeader } from "../util";
 
 export async function getEvents() {
-  const token = cookies().get("token");
-
-  const res = await axios.get(`/events`, {
+  const { status, data } = await axios.get(`/events`, {
     headers: buildAuthorizationHeader(),
   });
-  if (!res.status) {
+  if (status !== 200) {
     throw Error("Failed to fetch data");
   }
 
-  return res.data;
+  return data;
 }
