f1e1151af9b3df9f24ae7683f7c78b2df83b40f6
shomrkm <shochan1985@gmail.com>
2023-09-17 10:25:53 +0900

    feat: add base page to create new event.

---
+++ app/src/app/events/create/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default async function Page() {
+  return (
+    <main className="flex-col min-h-screen w-max">
+      <h1 className="text-4xl mb-4">Create Event</h1>
+    </main>
+  );
+}
