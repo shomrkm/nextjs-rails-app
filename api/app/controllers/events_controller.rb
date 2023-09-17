6ab3d918b01ebc200d7d9e4b224186122762bd92
shomrkm <shochan1985@gmail.com>
2023-09-17 13:48:07 +0900

    refact: refactoring association.

--- api/app/controllers/events_controller.rb
+++ api/app/controllers/events_controller.rb
@@ -1,7 +1,7 @@
 class EventsController < ApplicationController
   def index
     if current_user
-      events = current_user.events
+      events = current_user.created_events
       events = [] unless events
       render json: events, status: :ok
     else
