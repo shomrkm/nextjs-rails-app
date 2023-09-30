9c7df9ce9f635e41aef61eabad5a73317f640ac4
shomrkm <shochan1985@gmail.com>
2023-09-30 10:56:18 +0900

    refact: refactoring FieldWrapper component.

--- app/src/components/organisms/Form/FieldWrapper.tsx
+++ app/src/components/organisms/Form/FieldWrapper.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import { FieldError, Merge } from 'react-hook-form';
+import clsx from "clsx";
+import React from "react";
+import { FieldError, Merge } from "react-hook-form";
 
 type Props = {
   label?: string;
@@ -8,7 +9,10 @@ type Props = {
   error?: FieldError | Merge<FieldError, undefined> | undefined;
 };
 
-export type FieldWrapperPassThroughProps = Omit<Props, 'className' | 'children'>;
+export type FieldWrapperPassThroughProps = Omit<
+  Props,
+  "className" | "children"
+>;
 
 export const FieldWrapper: React.VFC<Props> = (props: Props) => {
   const { label, className, children, error } = props;
@@ -16,10 +20,14 @@ export const FieldWrapper: React.VFC<Props> = (props: Props) => {
     <div>
       <label className={`block text-sm font-medium text-gray-700 ${className}`}>
         {label}
-        <div className="mt-1">{children}</div>
+        <div className={clsx(label && "mt-1")}>{children}</div>
       </label>
       {error?.message && (
-        <div role="alert" aria-label={error.message} className="text-sm font-semibold text-red-500">
+        <div
+          role="alert"
+          aria-label={error.message}
+          className="text-sm font-semibold text-red-500"
+        >
           {error.message}
         </div>
       )}
