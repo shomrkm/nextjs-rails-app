cc4bbcb26619e5f8c5c69034bc9e118200709969
shomrkm <shochan1985@gmail.com>
2024-01-06 19:57:17 +0900

    test: add rspec for model/task.

--- api/spec/models/task_spec.rb
+++ api/spec/models/task_spec.rb
@@ -1,5 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    let(:task) { build(:task) }
+
+    it 'is valid with valid attributes' do
+      expect(task).to be_valid
+    end
+
+    it 'is not valid without a title' do
+      task.title = nil
+      expect(task).not_to be_valid
+    end
+
+    it 'is not valid with title longer than 50 characters' do
+      task.title = 'a' * 51
+      expect(task).not_to be_valid
+    end
+
+    it 'is not valid with description longer than 500 characters' do
+      task.description = 'a' * 501
+      expect(task).not_to be_valid
+    end
+
+    it 'is not valid without a status' do
+      task.status = nil
+      expect(task).not_to be_valid
+    end
+
+  end
 end
