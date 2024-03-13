f105b597053d5b1ccb6bc8c6953cee053e1e53eb
shomrkm <shochan1985@gmail.com>
2024-03-13 21:00:32 +0900

    feat: Photo,Taggingを追加

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_01_06_034508) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_13_114640) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,11 @@ ActiveRecord::Schema[7.0].define(version: 2024_01_06_034508) do
     t.datetime "updated_at", null: false
     t.boolean "archived"
     t.index ["owner_id"], name: "index_events_on_owner_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "tasks", force: :cascade do |t|
