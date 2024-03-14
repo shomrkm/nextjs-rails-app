ee36d3150c7185826685cb1146f7f45d153b2f1b
shomrkm <shochan1985@gmail.com>
2024-03-14 21:01:34 +0900

    Photo/Tagモデルを更新

--- api/app/models/photo.rb
+++ api/app/models/photo.rb
@@ -1,4 +1,20 @@
 class Photo < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings
+
+  def self.tagged_with(tag, *others, any: false)
+    tags = [tag, *others]
+
+    joins(:tags)
+      .merge(reflect_on_association(:tags).klass.with_name(tags))
+      .merge(
+        if tags.size == 1
+          all
+        elsif any
+          distinct
+        else
+          group(primary_key).having("COUNT(*) = ?", tags.size)
+        end
+      )
+  end
 end
--- api/app/models/tag.rb
+++ api/app/models/tag.rb
@@ -1,4 +1,8 @@
 class Tag < ApplicationRecord
   has_many :taggings
   has_many :photos, through: :taggings
+
+  def self.with_names(names)
+    where(name: names)
+  end
 end
