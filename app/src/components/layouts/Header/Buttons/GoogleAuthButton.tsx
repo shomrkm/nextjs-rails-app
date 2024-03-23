765601b6888326b3fc9cab4b95af3522792cd834
shomrkm <shochan1985@gmail.com>
2024-03-23 11:11:21 +0900

    refact: ログインはclient component なのでURLの分岐を削除

--- app/src/components/layouts/Header/Buttons/GoogleAuthButton.tsx
+++ app/src/components/layouts/Header/Buttons/GoogleAuthButton.tsx
@@ -6,20 +6,21 @@ import React from "react";
 export const GoogleAuthButton = () => {
   const handleAuthClick = async () => {
     await getSession();
-    // 仮のフォームを作成
+
+    // axios(ajax) を使って認証リクエストをAPIに送ると、Google認証ページにリダイレクトする際にCORSエラーとなる。
+    // そのため、仮のフォームを作成し、form.submit()で送信している。
     const form = document.createElement("form");
     form.method = "POST";
-    const baseURL =
-      typeof window === "undefined"
-        ? process.env.NEXT_SERVER_API_URL
-        : process.env.NEXT_PUBLIC_API_URL;
+    const baseURL = process.env.NEXT_PUBLIC_API_URL;
     form.action = `${baseURL}/auth/google_oauth2`;
 
     const csrfInput = document.createElement("input");
     csrfInput.type = "hidden";
+    // Rails の ActionController::RequestForgeryProtection では、
+    // authenticity_token という名前のパラメータをCSRF TOKEN として探すので、以下のように指定している
     csrfInput.name = "authenticity_token";
 
-    // CSRFトークンをセット（Railsで生成されたトークンを使用）
+    // getSettion()でCookieにセットされた CSRF-TOKEN を取得し、フォームにセットする
     const csrfToken = document.cookie
       ?.split(": ")
       .find((row) => row.startsWith("CSRF-TOKEN="))
@@ -29,7 +30,6 @@ export const GoogleAuthButton = () => {
         "Failed to login with google becauce CSRF-TOKEN was not found"
       );
     }
-
     csrfInput.value = csrfToken;
     form.appendChild(csrfInput);
 
